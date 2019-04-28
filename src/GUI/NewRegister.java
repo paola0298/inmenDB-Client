36402e997065a437fd98fa95518e0618d5b4e899
Hazel <hazel.martilo@gmail.com>
2019-04-27 23:00:53 -0600

    interfaz para la ventana de creacion de registros
    Se crea la primera version de la ventana de creacion de registros

---
+++ src/GUI/NewRegister.java
@@ -0,0 +1,49 @@
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class NewRegister {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button buttonEnter;
+
+    @FXML
+    private ScrollPane scrollPaneRegister;
+
+    @FXML
+    private AnchorPane anchorPaneRegister;
+
+    @FXML
+    private SplitPane splitPaneRegister;
+
+    @FXML
+    private VBox vBoxLabel;
+
+    @FXML
+    private VBox vBoxTextField;
+
+    @FXML
+    void boton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
