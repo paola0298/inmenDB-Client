d81626ebf064abdd51c4ae8ee2bbad207e9b02e2
paola0298 <pvillegasch@gmail.com>
2019-04-29 17:38:30 -0600

    Cambio en los menus contextuales

--- src/GUI.java
+++ src/GUI.java
@@ -4,32 +4,24 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class GUI extends Application implements EventHandler<ActionEvent>{
-    VBox scheme = new VBox();
-    StackPane mainWindowLayout = new StackPane();
-    GridPane grid = new GridPane();
-    Button newscheme = new Button();
-    HBox titlebutton = new HBox();
-
-    HBox newSchemeContainer;
-
-    MenuItem editScheme = new MenuItem("Editar Esquema...");
-    MenuItem deleteScheme = new MenuItem("Eliminar Esquema");
+public class GUI extends Application{
+    private VBox scheme = new VBox();
+    private StackPane mainWindowLayout = new StackPane();
+    private GridPane grid = new GridPane();
+    private Button newscheme = new Button();
+    private HBox titlebutton = new HBox();
+    private HBox newSchemeContainer;
 
 
 
 
-
-
-
-    public void start(Stage stage){
-
-
-        stage.setTitle("In Memory Data Base");
+    public void start(Stage stage) {
+        stage.setTitle("In Memory DataBase");
 
         /**Panel de la pantalla principal, aquí se van a
          * agregar los componentes de esta pantalla
@@ -38,24 +30,12 @@ public class GUI extends Application implements EventHandler<ActionEvent>{
 
         addtoMaingrid(mainWindowLayout);
 
-
-
-
-
-
-        Scene scene = new Scene(mainWindowLayout,1280,900);
+        Scene scene = new Scene(mainWindowLayout, 1280, 900);
         stage.setMinWidth(640);
         stage.setMinHeight(480);
 
         stage.setScene(scene);
-
-
-
-
-
-
         stage.show();
-
 
     }
 
@@ -68,11 +48,7 @@ public class GUI extends Application implements EventHandler<ActionEvent>{
         grid.setHgap(8);
         grid.setVgap(8);
         grid.setPadding(new Insets(10,10,10,10));
-
-
-
         addtoSchemeVBox(grid);
-
         mainWindowLayout.getChildren().addAll(grid);
 
     }
@@ -88,7 +64,7 @@ public class GUI extends Application implements EventHandler<ActionEvent>{
         // Se crea un HBox para poner el título y el botón horizontalmente
         Label schemetitle = new Label("Esquemas");
         newscheme.setText("Nuevo Esquema"); //Botón para agregar un nuevo esquema
-        newscheme.setOnAction(this);
+//        newscheme.setOnAction(this);
 
 
         titlebutton.setSpacing(15);
@@ -99,29 +75,32 @@ public class GUI extends Application implements EventHandler<ActionEvent>{
         //Aquí se agregan los componentes al VBox
         scheme.getChildren().addAll(titlebutton);
 
-
         //Ejemplo de como agregar un nuevo esquema al área destinada a mostrar los esquemas actuales
         addSchemeTitle(scheme, "hola");
         addSchemeTitle(scheme, "holaw");
 
-
-
-
         //Se agrega el VBox al GridPane
         grid.add(scheme,0,0);
-
-
-
-
-
-
-
     }
 
     public void addSchemeTitle(VBox scheme, String name) {
         Label schemetitle = new Label(name);
         Button edition = new Button("Editar");
         ContextMenu edit = new ContextMenu();
+        MenuItem editScheme = new MenuItem("Editar Esquema...");
+        editScheme.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Editar un esquema");
+            }
+        });
+        MenuItem deleteScheme = new MenuItem("Eliminar Esquema");
+        deleteScheme.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Eliminar un esquema");
+            }
+        });
         edit.getItems().addAll(editScheme, deleteScheme);
         edition.setContextMenu(edit);
 
@@ -137,19 +116,19 @@ public class GUI extends Application implements EventHandler<ActionEvent>{
 
     }
 
-    @Override
-    /**
-     * En esta clase se manejarán las acciones por hacer de los botones
-     * @author Brayan Rodríguez Villalobos
-     */
-    public void handle(ActionEvent actionEvent) {
-        if (actionEvent.getSource()==newscheme){
-            //Abrir la ventana para crear un nuevo esquema
-            //Actualizar el espacio donde se muestran los esquemas ya hechos
-            System.out.println("Nueva pantalla");
-        }//else if (actionEvent.getSource()){
-//
-//
-//        }
-    }
+//    @Override
+//    /**
+//     * En esta clase se manejarán las acciones por hacer de los botones
+//     * @author Brayan Rodríguez Villalobos
+//     */
+//    public void handle(ActionEvent actionEvent) {
+//        if (actionEvent.getSource()==newscheme){
+//            //Abrir la ventana para crear un nuevo esquema
+//            //Actualizar el espacio donde se muestran los esquemas ya hechos
+//            System.out.println("Nueva pantalla");
+//        }//else if (actionEvent.getSource()){
+////
+////
+////        }
+//    }
 }
