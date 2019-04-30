3cdd42f6b1608168ffca3487536e44574d8b60c4
Hazel <hazel.martilo@gmail.com>
2019-04-30 00:33:04 -0600

    Cambios de algunos metodos 1

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,23 +1,35 @@
 package GUI;
 
+import com.google.gson.Gson;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
+
+        String stringPrueba;
+        stringPrueba=JOptionPane.showInputDialog(null,"inserte json");
+        JSON prueba = new JSON();
+        prueba.recive_data(stringPrueba);
         launch(args);
+
     }
 
     @Override
     public void start(Stage primaryStage)throws Exception {
+
         Parent root = FXMLLoader.load(getClass().getResource("NewRegister.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setTitle("Create New Register");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
 
     }
 }
+
+
--- src/GUI/NewRegister.java
+++ src/GUI/NewRegister.java
@@ -4,19 +4,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.SplitPane;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-
+import javafx.scene.text.Font;
 public class NewRegister {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private Button buttonEnter;
@@ -31,19 +24,39 @@ public class NewRegister {
     private SplitPane splitPaneRegister;
 
     @FXML
-    private VBox vBoxLabel;
+    private TextField nameTextFIeld;
 
     @FXML
-    private VBox vBoxTextField;
+    private TextField sizeTextField;
 
     @FXML
-    void boton(ActionEvent event) {
+    private TextField typeTextField;
+
+    @FXML
+    private TextField primaryKeyTextFIeld;
+
+    private Label labelTest;
+
+    public void createLabel(String setText){
+        Label labelCreated = new Label("setText");
+        labelCreated.setMaxSize(80,50);
+        labelCreated.setAlignment(Pos.TOP_CENTER);
+        labelCreated.setText(setText);
+        labelCreated.setFont(Font.font("Verdana",20));
+
+
 
     }
 
     @FXML
-    void initialize() {
+    void boton(ActionEvent event) {
+        System.out.println("Button enter was pressed");
 
+    }
+
+
+    @FXML
+    void initialize() {
 
     }
 }
