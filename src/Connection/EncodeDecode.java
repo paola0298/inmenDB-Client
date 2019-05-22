a110216e7b7b365ff1f7b68366d417f3c04d7105
paola0298 <pvillegasch@gmail.com>
2019-05-22 17:34:19 -0600

    Clase para codificar y decodificar

---
+++ src/Connection/EncodeDecode.java
@@ -0,0 +1,20 @@
+package Connection;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class EncodeDecode {
+    public static String cifrarBase64(String a) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        String b = encoder.encodeToString(a.getBytes(StandardCharsets.UTF_8));
+        return b;
+    }
+
+    public static String descifrarBase64(String a) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] decodedByteArray = decoder.decode(a);
+
+        String b = new String(decodedByteArray);
+        return b;
+    }
+}
